f51af3dfac72b57ce5df7da808c825bb53f8458c
Usama Mehmood <usamamehmood62000@gmail.com>
2022-02-17 02:20:38 -0800

    first commit

---
+++ src/app/square/square.component.ts
@@ -0,0 +1,17 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-square',
+  templateUrl: './square.component.html',
+  styleUrls: ['./square.component.scss']
+})
+export class SquareComponent {
+
+  @Input() value!: 'X' | 'O';
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
